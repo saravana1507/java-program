d311384f531d546fb93a535e50b56e4041f0a647
saravana1507 <skvsaravana@gmail.com>
2023-05-24 22:06:43 +0530

    update

--- my_program/src/encapsulation/Mobile.java
+++ my_program/src/encapsulation/Mobile.java
@@ -1,34 +1,28 @@
 package encapsulation;
-
+import java.util.Scanner;
 public class Mobile extends Mobileshop {
 	int mobile_price;
 	String mobile_name;
 	String old_mobile;
 final static String mobile_shop="poorvika";
-static void  offer()
-{   System.out.println("     "+mobile_shop);
-	System.out.println("limited offer");
-}
 	Mobile() {
-		//System.out.println("       "+mobile_shop);
-		System.out.println(" mobile price list notice");
+		System.out.println("       "+mobile_shop);
+		System.out.println("-------------------------------");
+		System.out.println("limited offer");	
+		System.out.println("mobile price list notice");
 	}
 	Mobile(String old_mobile)
 	{
-        this();
-        offer();
 		this.old_mobile=old_mobile;
-		System.out.println("  OLD mobile = " + old_mobile +" exchanged");
+		System.out.println( old_mobile +" exchanged");
 		
 	}
 
 	Mobile(int mobile_price, String mobile_name) {
-		
-		offer();
 		this.mobile_name = mobile_name;
 		this.mobile_price = mobile_price;
-		System.out.println("  mobile name = " + mobile_name);
-		System.out.println("  mobile price = " + mobile_price);
+		System.out.println("mobile name = " + mobile_name);
+		System.out.println("mobile price = " + mobile_price);
 		if (mobile_price <= 10000 && mobile_price >= 3000) {
 			System.out.println(" -- offer one headphone --");
 		}
@@ -41,14 +35,16 @@ static void  offer()
 	}
 
 	static public void main(String arg[]) {
-		offer();
-		System.out.println("-------------------------------");
-		Mobile customer1 = new Mobile();
-		System.out.println("-------------------------------");
-		Mobile customer = new Mobile("vivo");
-		System.out.println("-------------------------------");
-		Mobile customer2 = new Mobile(14000, "samsung");
-		System.out.println("-------------------------------");
-		Mobile customer3 = new Mobile(74000, "apple");	
+		Mobile obj= new Mobile();
+		Scanner s =new Scanner(System.in);
+		System.out.print("old mobile name =");
+		String n= s.nextLine();
+		 obj=new Mobile(n);
+		System.out.print("new mobile=");
+		String name= s.nextLine();
+		System.out.print("amount =");
+		int amt= s.nextInt();
+		obj= new Mobile(amt, name);
+	    s.close();
 	}
 }
