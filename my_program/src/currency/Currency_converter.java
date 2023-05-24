3e4da2b1ff1a51c04bcd5f3afc5ccb6cbb3e7c4c
saravana1507 <skvsaravana@gmail.com>
2023-05-24 22:04:48 +0530

    update

--- my_program/src/currency/Currency_converter.java
+++ my_program/src/currency/Currency_converter.java
@@ -1,15 +1,29 @@
 package currency;
-
+import java.util.Scanner;
 public class Currency_converter extends Difference_money{
 
 	public static void main(String[] args) {
      Money currency=new Currency_converter();
-     currency.usd_to_rupee(5);
-     currency.rupee_to_usd(300);
-     currency.euro_to_rupee(9);
-     currency.rupee_to_euro(543);
-     currency.usd_to_euro(12);
-     currency.euro_to_usd(17);
+     Scanner scan=new Scanner(System.in);
+     System.out.print("enter usd value = ");
+     int a= scan.nextInt();
+     currency.usd_to_rupee(a);
+     System.out.print("enter rupee value = ");
+     int b= scan.nextInt();
+     currency.rupee_to_usd(b);
+     System.out.print("enter euro value = ");
+     int c= scan.nextInt();
+     currency.euro_to_rupee(c);
+     System.out.print("enter rupee value = ");
+     int d= scan.nextInt();
+     currency.rupee_to_euro(d);
+     System.out.print("enter usd value = ");
+     int e= scan.nextInt();
+     currency.usd_to_euro(e);
+     System.out.print("enter euro value = ");
+     int f= scan.nextInt();
+     currency.euro_to_usd(f);
+     scan.close();
      
 	}
 
