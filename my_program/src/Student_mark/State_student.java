8960f8199cb8fe01b82fb53f32c9a652a97497d9
saravana1507 <skvsaravana@gmail.com>
2023-05-29 23:43:08 +0530

    update

---
+++ my_program/src/Student_mark/State_student.java
@@ -0,0 +1,10 @@
+package Student_mark;
+
+public interface State_student {
+void head();
+void detail();
+void mark();
+void display();
+void mark_detail();
+void check();
+}
---
+++ my_program/src/Student_mark/Student_list.java
@@ -0,0 +1,12 @@
+package Student_mark;
+
+public class Student_list extends Student {
+
+	public static void main(String[] args) {
+		State_student obj = new Student_list(); 
+	//	obj.mark();
+		obj.mark_detail();
+		obj.check();
+	}
+
+}
